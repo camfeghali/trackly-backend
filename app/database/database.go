662332fa30b345ac7ee2a7d59e6ac7d1e32e1900
Camille Feghali <camfeg@gmail.com>
2021-05-08 16:01:24 +0200

    split code up

--- app/database/database.go
+++ app/database/database.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"fmt"
-	"log"
+	"trackly-backend-old/app/utils"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,8 +15,6 @@ type DB struct {
 func NewDBInstance(db_name, db_user, db_pw string) *DB {
 	dsn := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/%v?charset=utf8mb4&parseTime=True&loc=Local", db_name, db_user, db_pw)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
+	utils.CheckError(err)
 	return &DB{db}
 }
--- app/main/main.go
+++ app/main/main.go
@@ -1,32 +1,40 @@
 package main
 
-import "trackly-backend-gorm/app/database"
-
-type User struct {
-	Id        int
-	FirstName string
-	LastName  string
-}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"trackly-backend/app/database"
+	"trackly-backend/app/models"
+)
 
 func main() {
+	var port string = "8080"
 	db := database.NewDBInstance("trackly_user", "insabgho123", "trackly")
 	// Migrate the schema
 	// db.AutoMigrate(&Product{})
 
 	// Create
-	db.Create(&User{FirstName: "D42", LastName: "Doodoo"})
+	db.Create(&models.User{FirstName: "D42", LastName: "Doodoo"})
 
 	// Read
-	var user User
+	var user models.User
 	// db.First(&user)                            // find user with integer primary key
-	db.First(&user, "last_name = ?", "Doodoo") // find user with code D42
+	// db.First(&user, "last_name = ?", "Doodoo") // find user with code D42
 
-	// Update - update user's price to 200
-	db.Model(&user).Update("last_name", "Kathy")
-	// Update - update multiple fields
-	db.Model(&user).Updates(User{FirstName: "Emmanuel", LastName: "Thuring"}) // non-zero fields
-	db.Model(&user).Updates(map[string]interface{}{"first_name": "Olala", "last_name": "F42"})
+	// // Update - update user's price to 200
+	// db.Model(&user).Update("last_name", "Kathy")
+	// // Update - update multiple fields
+	// db.Model(&user).Updates(User{FirstName: "Emmanuel", LastName: "Thuring"}) // non-zero fields
+	// db.Model(&user).Updates(map[string]interface{}{"first_name": "Olala", "last_name": "F42"})
 
 	// Delete - delete user
 	db.Delete(&user, 42)
+
+	fmt.Printf("Serving on port: %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from root")
 }
