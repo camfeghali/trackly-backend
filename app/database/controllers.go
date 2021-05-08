2be2a18d7abbd769f45b5a23976d3c9dfabc8ff9
Camille Feghali <camfeg@gmail.com>
2021-05-08 21:10:04 +0200

    handle error from db

--- app/database/controllers.go
+++ app/database/controllers.go
@@ -1,9 +1,11 @@
 package database
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 	"trackly-backend/app/models"
+
+	"trackly-backend/app/utils"
 
 	"github.com/gorilla/mux"
 )
@@ -11,14 +13,21 @@ import (
 func (db *DB) GetUser(w http.ResponseWriter, r *http.Request) {
 	user := models.User{}
 	params := mux.Vars(r)
-	db.First(&user, params["id"])
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(user)
+	// db.First(&user, params["id"])
+	if db_resp := db.First(&user, params["id"]); db_resp.Error != nil {
+		// Create failed, do something e.g. return, panic etc.
+		utils.ErrorResponse(w, 200, db_resp.Error.Error())
+
+		fmt.Println(db_resp.Error)
+		return
+	}
+	utils.JsonResponse(w, 200, user)
 }
 
 func (db *DB) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	var users []models.User
 	db.Find(&users)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(users)
+	utils.JsonResponse(w, 200, users)
 }
--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,9 +1,22 @@
 package utils
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func CheckError(err error) {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func JsonResponse(w http.ResponseWriter, code int, toJson interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(toJson)
+}
+
+func ErrorResponse(w http.ResponseWriter, code int, message string) {
+	JsonResponse(w, code, map[string]string{"error": message})
 }
